4c27adb462677d63e0cc47256a8e166005ce4ef5
redaphid <iam@aaronherres.com>
2022-10-15 20:07:42 -0400

    get gutenburg html index for book

--- scripts/get-sci-fi-corpus.js
+++ scripts/get-sci-fi-corpus.js
@@ -2,16 +2,17 @@
 
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
-import {existsSync, readFileSync, writeFileSync } from 'fs'
+import { existsSync, readFileSync, writeFileSync} from 'fs'
+import {readFile, writeFile } from 'fs/promises'
 import filenamifyUrl from 'filenamify-url';
 
 const phillipKDick = "https://www.gutenberg.org/ebooks/author/33399/"
 
 async function getBookListHtml(baseUrl) {
-    if(!baseUrl) throw new Error("no baseUrl")
+    if (!baseUrl) throw new Error("no baseUrl")
     const safeName = filenamifyUrl(baseUrl)
 
-    if(existsSync(`tmp/${safeName}.html`)) {
+    if (existsSync(`tmp/${safeName}.html`)) {
         console.log('html existed')
         return readFileSync(`tmp/${safeName}.html`, 'utf8')
     }
@@ -28,14 +29,29 @@ async function getBookList(baseUrl) {
     const lElems = dom.window.document.querySelectorAll('.results li a')
     let links = []
     lElems.forEach(l => links.push(l.getAttribute('href')))
-    console.log('links',links)
     const bookUrlRegex = new RegExp(/ebooks\/[0-9]+/)
     links = links.filter(l => bookUrlRegex.test(l))
     links = links.map(l => `https://www.gutenberg.org${l}`)
-    console.log('links', links)
     return links
 }
 
+const getBookHtml = async (url) => {
+    const safeName = filenamifyUrl(url)
+    if (existsSync(`tmp/${safeName}.html`)) {
+        console.log('book html existed')
+        return await readFile(`tmp/${safeName}.txt`, 'utf8')
+    }
+    console.log('crawling new book html')
+    const res = await fetch(url)
+    const text = await res.text()
+    await writeFile(`tmp/${safeName}.html`, text, 'utf8')
+}
+
+const getBookText = async (url) => {
+    const html = await getBookHtml(url)
+    console.log({html})
+    return html
+}
 async function getScript(url) {
     url = baseUrl + url
     const res = await fetch(url)
@@ -51,8 +67,8 @@ async function getScript(url) {
 //     return script
 // }
 async function getBooks() {
-    const episodeList = await getBookList(phillipKDick)
-    // const promises = map(episodeList, getScript)
+    const bookList = await getBookList(phillipKDick)
+    const bookTexts = await Promise.all(bookList.map(getBookText))
     // return await Promise.all(promises)
 }
 
