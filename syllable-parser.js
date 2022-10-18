1daf157cac220eb706fefe6e0ecdd183f1844767
redaphid <iam@aaronherres.com>
2022-10-18 17:54:06 -0400

    lowercase

--- syllable-parser.js
+++ syllable-parser.js
@@ -1,7 +1,7 @@
 const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/gi
 
 export const syllabify = (words) => {
-  return words.match(syllableRegex)
+  return words.match(syllableRegex).map(syllable => syllable.toLowerCase())
 }
 
 export default syllabify
--- syllable-parser.test.js
+++ syllable-parser.test.js
@@ -5,7 +5,14 @@ describe('syllabify', () => {
   it('should return an array of syllables', () => {
     const words = 'syllable'
     const syllables = syllabify(words).sort()
-    console.log({syllables})
     expect(syllables).toEqual(['syl', 'lab', 'le'].sort())
+  })
+
+  describe("When the word has capital letters", () => {
+    it('should return an array of lower-case syllables', () => {
+      const words = 'Syllable'
+      const syllables = syllabify(words).sort()
+      expect(syllables).toEqual(['syl', 'lab', 'le'].sort())
+    })
   })
 })
