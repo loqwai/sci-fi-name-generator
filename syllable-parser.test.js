a995dbacde5c1c6e3dbe7925baa7ad945098ce06
redaphid <iam@aaronherres.com>
2022-10-18 17:38:29 -0400

    lazy way of array equality checking

--- syllable-parser.test.js
+++ syllable-parser.test.js
@@ -4,7 +4,8 @@ import {syllabify} from './syllable-parser.js'
 describe('syllabify', () => {
   it('should return an array of syllables', () => {
     const words = 'syllable'
-    const syllables = syllabify(words)
-    expect(syllables).toEqual(['syll', 'a', 'ble'])
+    const syllables = syllabify(words).sort()
+    console.log({syllables})
+    expect(syllables).toEqual(['syl', 'lab', 'le'].sort())
   })
 })
