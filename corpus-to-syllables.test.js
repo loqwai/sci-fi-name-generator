c92b34a16c71190de2bc0ddeadf383adb3005e87
redaphid <iam@aaronherres.com>
2022-10-19 13:24:01 -0400

    test on folder

--- corpus-to-syllables.test.js
+++ corpus-to-syllables.test.js
@@ -1,10 +1,24 @@
-import {corpusToSyllables} from './corpus-to-syllables.js'
+import { corpusToSyllables } from './corpus-to-syllables.js'
 
 describe('corpusToSyllables', () => {
-  it('should return an array of syllables', async () => {
-    const path = './test_data/some-file.txt'
-    console.log('in corpus test')
-    const syllables = await corpusToSyllables(path)
-    expect(syllables.length).toBeGreaterThan(5)
+  describe('when the path is a single file', () => {
+    let singleFileSyllables
+    beforeEach(async () => {
+      const path = './test_data/some-file.txt'
+      singleFileSyllables = await corpusToSyllables(path)
+    })
+    it('should return an array of syllables', () => {
+      expect(singleFileSyllables.length).toBeGreaterThan(5)
+    })
+    describe('when the path is a directory that contains that file', () => {
+      let directorySyllables
+      beforeEach(async () => {
+        const path = './test_data'
+        directorySyllables = await corpusToSyllables(path)
+      })
+      it('should return an array of syllables that is larger than the single file', () => {
+        expect(directorySyllables.length).toBeGreaterThan(singleFileSyllables.length)
+      })
+    })
   })
 })
