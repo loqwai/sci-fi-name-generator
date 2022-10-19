fc980e5447d82ee12e0e1cb2695ba62522b3ecf2
redaphid <iam@aaronherres.com>
2022-10-19 12:44:51 -0400

    remove non letters

--- syllable-parser.js
+++ syllable-parser.js
@@ -1,6 +1,8 @@
 const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/gi
 
+const removeNonLetters = (word) => word.replace(/[^a-z]/gi, '')
 export const getSyllables = (word) => {
+  word = removeNonLetters(word)
   return word.match(syllableRegex).map(syllable => syllable.toLowerCase())
 }
 export default getSyllables
