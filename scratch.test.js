260d4cc94734dc708e317170e2d1952f341642b5
redaphid <iam@aaronherres/com>
2022-10-28 23:15:43 -0700

    add generated syllable json

--- scratch.test.js
+++ scratch.test.js
@@ -1,4 +1,5 @@
 import { corpusToSyllables } from './corpus-to-syllables.js'
+import {writeFileSync} from 'fs'
 
 const randomInRange = (min, max) => {
   // thanks to copilot, that cited the actual source
@@ -16,15 +17,21 @@ const wordFromSyllables = (syllables) => {
 }
 describe('When using the test harness to run the program', () => {
   describe('when the path is the phillipkdick folder', () => {
-    let singleFileSyllables
+    let phillipSyllables
     beforeEach(async () => {
       const path = './data/phillip_k_dick'
-      singleFileSyllables = await corpusToSyllables(path)
+      phillipSyllables = await corpusToSyllables(path)
+      // writeFileSync('./data/phillip_syllables.json', JSON.stringify(phillipSyllables, null, 2))
     })
     it('should return an array of syllables', () => {
-      expect(singleFileSyllables.length).toBeGreaterThan(5)
-      const manyWords = new Array(100).fill(0).map(() => wordFromSyllables(singleFileSyllables))
+      expect(phillipSyllables.length).toBeGreaterThan(5)
+      const manyWords = new Array(10).fill(0).map(() => wordFromSyllables(phillipSyllables))
       console.log(manyWords.join('\n'))
     })
   })
 })
+const okOptions = [
+  'tentani',
+  'boungof',
+  'valthec',
+]
