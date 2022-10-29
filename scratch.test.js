be1e69f3eb2fc1da3a49a0613a44a4b0cec7e14b
redaphid <iam@aaronherres/com>
2022-10-28 23:05:05 -0700

    just generate random shit from the phillipkdick corpus and see if that's good enough

--- scratch.test.js
+++ scratch.test.js
@@ -1,5 +1,19 @@
 import { corpusToSyllables } from './corpus-to-syllables.js'
 
+const randomInRange = (min, max) => {
+  // thanks to copilot, that cited the actual source
+  // https://stackoverflow.com/a/1527820/10840
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const wordFromSyllables = (syllables) => {
+  const syllableCount = syllables.length
+  const wordLength = randomInRange(1, 4)
+  const word = new Array(wordLength).fill(0).map(() => {
+    const syllableIndex = randomInRange(0, syllableCount - 1)
+    return syllables[syllableIndex]
+  })
+  return word.join('')
+}
 describe('When using the test harness to run the program', () => {
   describe('when the path is the phillipkdick folder', () => {
     let singleFileSyllables
@@ -8,8 +22,9 @@ describe('When using the test harness to run the program', () => {
       singleFileSyllables = await corpusToSyllables(path)
     })
     it('should return an array of syllables', () => {
-      console.log(singleFileSyllables)
       expect(singleFileSyllables.length).toBeGreaterThan(5)
+      const manyWords = new Array(100).fill(0).map(() => wordFromSyllables(singleFileSyllables))
+      console.log(manyWords.join('\n'))
     })
   })
 })
